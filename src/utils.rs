443408450e0ddaf98b9a3e5ea8d963013cf1f350
Abir Siddiky <105973211+abirsiddiky@users.noreply.github.com>
2026-07-21 11:53:37 +0600

    Simplify file import in utils.rs
    Refactor import statements for clarity.

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,6 @@
 use anyhow::{Context, Result};
 use chrono::Local;
-use std::fs::{File, OpenOptions};
+use std::fs::OpenOptions;
 use std::io::Write;
 use std::path::Path;
 use std::process::Command;
